4bbbfa22f6f6a6e35a94b65d5963ea5732f1d4ad
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-10-02 03:12:33 +0000

    Create cell factory and populate board

--- js/index.js
+++ js/index.js
@@ -1,17 +1,37 @@
 // FACTORIES
 
 // MODULES
-const gameboard = () => {
+function Gameboard() {
    const board = []; // shall be 2D
    const rows = 3;
    const columns = 3;
-
+   
    for (let i = 0; i < rows; i++) {
+      board[i] = [];
       for (let j = 0; j < columns; j++) {
-         board[i][j] = Cell();
+         board[i][j] = Cell(i, j);
       }
    }
-
    
+   const getBoard = () => board;
+
+   return {
+      getBoard,
+   };
 }
 
+function Cell(row, column) {
+   const address = {
+      row,
+      column,  
+   };
+   let value = 0;
+
+   const getAddress = () => address;
+   const getVal = () => value;
+
+   return {
+      getAddress,
+      getVal,
+   };
+};
